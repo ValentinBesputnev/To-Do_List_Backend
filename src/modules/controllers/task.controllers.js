bd6418ba3b9ff4aae213d07b53a238758badb199
ValentinBesputnev <besputnev.exceedteam@gmail.com>
2022-04-04 12:53:56 +0300

    fix comments

--- src/modules/controllers/task.controllers.js
+++ src/modules/controllers/task.controllers.js
@@ -1,66 +1,61 @@
 const tasksSchema = require("../../schema/taskSchema");
 
-module.exports.getAllTasks = async (req, res) => {
+module.exports.getAllTasks = (req, res) => {
   try {
-    const allTasks = await tasksSchema.find();
-    res.send(allTasks);
+    tasksSchema.find().then((result) => {
+      res.send({ data: result });
+    });
   } catch (e) {
     res.status(500).send({ message: "Internal server error" });
   }
 };
 
-module.exports.createNewTask = async (req, res) => {
+module.exports.createNewTask = (req, res) => {
   try {
-    const { isCheck, text } = req.body;
-    console.log(req.body);
-    if (isCheck !== undefined && text) {
-      const newTask = new tasksSchema({
-        text,
-        isCheck
+    const task = new tasksSchema(req.body);
+    task.save().then(() => {
+      tasksSchema.find().then((result) => {
+        res.send({ data: result });
       });
-      await newTask.save().then(async (result) => {
-          await tasksSchema.find().then((r) => {
-            res.status(200).json(r);
-          });
-        }).catch((e) => {
-          res.status(400).json({ message: "Task dont saved" });
+    });
+  } catch (e) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+module.exports.changeTaskInfo = (req, res) => {
+  try {
+    const body = req.body;
+    const { text, isCheck, _id } = body;
+    if (
+      body.hasOwnProperty("_id") &&
+      (body.hasOwnProperty("text") || body.hasOwnProperty("isCheck"))
+    ) {
+      tasksSchema.updateOne({ _id }, body).then(() => {
+        tasksSchema.find().then((result) => {
+          res.send({ data: result });
         });
+      });
     } else {
-      res.status(400).json({ message: "invalid data" });
+      res.status(422).send("Error! Params not correct");
     }
   } catch (e) {
     res.status(500).send({ message: "Internal server error" });
   }
 };
 
-module.exports.changeTaskInfo = async (req, res) => {
-  try {
-    const { text, isCheck, _id } = req.body;
-    if (!req.body) return res.status(422).send({ message: "Error! Write correct data" });
-    await tasksSchema.updateOne({ _id }, { text, isCheck })
-      .then(async (result) => {
-        await tasksSchema.find().then((r) => {
-          res.status(200).json(r);
-        });
-      }).catch((e) => {
-        res.status(400).json({ message: "Something went wrong" });
-      });
-  } catch (e) {
-    res.status(500).send({ message: "Internal server error" });
-  }
-};
-
-module.exports.deleteTask = async (req, res) => {
+module.exports.deleteTask = (req, res) => {
   try {
     const { id } = req.query;
-    if (!req.query) return res.status(422).send({ message: "Error! Write correct Id" });
-    await tasksSchema.findByIdAndDelete(id).then(async (result) => {
-        await tasksSchema.find().then((r) => {
-          res.status(200).json(r);
+    if (id) {
+      tasksSchema.deleteOne({ _id: id }).then(() => {
+        tasksSchema.find().then((result) => {
+          res.send({ data: result });
         });
-      }).catch((e) => {
-        res.status(400).json({ message: "something went wrong" });
       });
+    } else {
+      res.status(422).send("Error! Params not correct");
+    }
   } catch (e) {
     res.status(500).send({ message: "Internal server error" });
   }
